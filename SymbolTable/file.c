afbfe1529f6076c56b19f61f6e43174c36ce787e
Shivanirudh <shivanirudh2001@gmail.com>
2021-02-12 21:22:14 +0530

    Symbol Table

---
+++ SymbolTable/file.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*Multiline
+comment*/
+main()
+{
+  float c = 20;
+  int a=10,b=20;
+  
+  if (a != b)
+      printf(“a is greater”);
+  else
+      printf(“b is greater”);
+}
+add()
+{
+  int a = 10;
+}
+//Single line comment
