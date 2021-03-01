521bdd09f4eb93d987104453e2a36cfea70e70c1
Shivanirudh <shivanirudh2001@gmail.com>
2021-03-01 21:28:31 +0530

    Recursive Descent Parser

--- A04-RecursiveDescentParser/parserext.c
+++ A04-RecursiveDescentParser/parserext.c
@@ -6,12 +6,51 @@ void tab(int val){
     while(val--)
         printf("\t");
 }
+int F(char *, int *, int);
+int Tprime(char *, int *, int);
+int T(char *, int *, int);
+int Eprime(char *, int *, int);
+int E(char *, int *, int);
 
-int F(char *str, int *look_ahead, int level){
+int main(){
+
+    char *str = (char*)calloc(100, sizeof(char));
+    printf("\nEnter string to parse: ");
+    scanf(" %s", str);
+    strcat(str, "$");
+    int look_ahead = 0;
+    
+    printf("----------------------------\n");
+    printf("Enter E\n");
+    E(str, &look_ahead, 1);
+    printf("Exit E\n");
+    printf("----------------------------\n");
+    
+    if (str[look_ahead] == '$')
+        printf("\nSuccess");
+    else
+        printf("\nFailure: %c at position %d not expected. \n", str[look_ahead], look_ahead);
+}
+
+int F(char *str, int *look_ahead, int level)
+{
     if (str[*look_ahead] == 'i')
     {
         tab(level);
         printf("F: i matched\n");
+        (*look_ahead)++;
+    }
+    else if (str[*look_ahead] == '(')
+    {
+        tab(level);
+        printf("F: ( matched\n");
+        (*look_ahead)++;
+        E(str, look_ahead, level + 1);
+    }
+    else if (str[*look_ahead] == ')')
+    {
+        tab(level);
+        printf("F: ) matched\n");
         (*look_ahead)++;
     }
 }
@@ -20,6 +59,32 @@ int Tprime(char *str, int *look_ahead, int level){
     if(str[*look_ahead] == '*'){
         tab(level);
         printf("T': * matched\n");
+        (*look_ahead)++;
+
+        tab(level);
+        printf("----------------------------\n");
+        tab(level);
+        printf("Enter F\n");
+        F(str, look_ahead, level + 1);
+        tab(level);
+        printf("Exit F\n");
+        tab(level);
+        printf("----------------------------\n");
+
+        tab(level);
+        printf("----------------------------\n");
+        tab(level);
+        printf("Enter T'\n");
+        Tprime(str, look_ahead, level + 1);
+        tab(level);
+        printf("Exit T'\n");
+        tab(level);
+        printf("----------------------------\n");
+        
+    }
+    else if(str[*look_ahead] == '/'){
+        tab(level);
+        printf("T': / matched\n");
         (*look_ahead)++;
 
         tab(level);
@@ -93,7 +158,33 @@ int Eprime(char *str, int *look_ahead, int level){
         tab(level);
         printf("----------------------------\n");
     }
+    else if(str[*look_ahead] == '-'){
+        tab(level);
+        printf("E': - matched\n");
+        (*look_ahead)++;
+        
+        tab(level);
+        printf("----------------------------\n");
+        tab(level);
+        printf("Enter T\n");
+        T(str, look_ahead, level + 1);
+        tab(level);
+        printf("Exit T\n");
+        tab(level);
+        printf("----------------------------\n");
+
+        tab(level);
+        printf("----------------------------\n");
+        tab(level);
+        printf("Enter E'\n");
+        Eprime(str, look_ahead, level + 1);
+        tab(level);
+        printf("Exit E'\n");
+        tab(level);
+        printf("----------------------------\n");
+    }
 }
+
 int E(char *str, int *look_ahead, int level){
     tab(level);
     printf("----------------------------\n");
@@ -114,35 +205,4 @@ int E(char *str, int *look_ahead, int level){
     printf("Exit E'\n");
     tab(level);
     printf("----------------------------\n");
-}
-
-int main(){
-    /*char *prod_set[100];
-    for (int i = 0; i < 100;i++)
-        prod_set[i] = (char *)calloc(100, sizeof(char));
-
-    int prod_ctr;
-    printf("\nEnter number of productions in grammar: ");
-    scanf("%d", &prod_ctr);
-    printf("\nEnter grammar: ");
-    for (int i = 0; i < prod_ctr;i++){
-        scanf(" %s", prod_set[i]);
-    }*/
-
-    char *str = (char*)calloc(100, sizeof(char));
-    printf("\nEnter string to parse: ");
-    scanf(" %s", str);
-    strcat(str, "$");
-    int look_ahead = 0;
-    
-    printf("----------------------------\n");
-    printf("Enter E\n");
-    E(str, &look_ahead, 1);
-    printf("Exit E\n");
-    printf("----------------------------\n");
-    
-    if (str[look_ahead] == '$')
-        printf("\nSuccess");
-    else
-        printf("\nFailure: %c at position %d not expected. \n", str[look_ahead], look_ahead);
 }
